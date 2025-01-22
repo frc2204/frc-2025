97f1aee0f11d6c078a48407fdbe1ce3146cd07c2
pep233 <yibai14651@gmail.com>
2025-01-21 17:16:31 -0800

    fixed imports

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -48,7 +48,6 @@ import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.Constants;
 import frc.robot.Constants.Mode;
 import frc.robot.generated.TunerConstants;
-import frc.robot.subsystems.GyroIOInputsAutoLogged;
 import frc.robot.util.LocalADStarAK;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
