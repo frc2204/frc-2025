1cc6dffef4ace0dfcbdfa591487684be187b6e64
pep233 <yibai14651@gmail.com>
2025-02-08 14:04:14 -0800

    Position elevator command

--- src/main/java/frc/robot/commands/elevator/PositionElevator.kt
+++ src/main/java/frc/robot/commands/elevator/PositionElevator.kt
@@ -1,4 +1,19 @@
 package frc.robot.commands.elevator
 
-class PositionElevator {
+import edu.wpi.first.wpilibj2.command.Command
+import frc.robot.subsystems.elevator.ElevatorSubsystem
+
+class PositionElevator(private val position: () -> Double, private val endCondition: (position: Double) -> Boolean)
+    : Command() {
+    init {
+        addRequirements(ElevatorSubsystem)
+    }
+
+    override fun initialize() {
+        ElevatorSubsystem.desiredPosition = position.invoke()
+    }
+
+    override fun isFinished(): Boolean {
+        return endCondition.invoke(ElevatorSubsystem.position)
+    }
 }
