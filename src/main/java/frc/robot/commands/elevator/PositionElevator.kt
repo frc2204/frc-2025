285ec366a9d02cf20a1ac2079d3154fab51cb95c
pep233 <yibai14651@gmail.com>
2025-02-12 16:48:03 -0800

    added secondary constructor for PositionElevator.kt

--- src/main/java/frc/robot/commands/elevator/PositionElevator.kt
+++ src/main/java/frc/robot/commands/elevator/PositionElevator.kt
@@ -5,10 +5,12 @@ import frc.robot.subsystems.elevator.ElevatorSubsystem
 
 class PositionElevator(private val position: () -> Double, private val endCondition: (position: Double) -> Boolean)
     : Command() {
+
+    constructor(position: () -> Double) : this(position, {true})
+
     init {
         addRequirements(ElevatorSubsystem)
     }
-
     override fun initialize() {
         ElevatorSubsystem.desiredPosition = position.invoke()
     }
