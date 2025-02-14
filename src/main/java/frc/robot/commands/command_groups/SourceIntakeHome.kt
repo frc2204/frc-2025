cb94b8fc979aedc1592e9ce6cb4433cb20834f3e
Jiale Yu <yibai14651@gmail.com>
2025-02-13 17:56:22 -0800

    added source intake & source intake home

--- src/main/java/frc/robot/commands/command_groups/SourceIntakeHome.kt
+++ src/main/java/frc/robot/commands/command_groups/SourceIntakeHome.kt
@@ -6,7 +6,7 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
 import frc.robot.commands.elevator.PositionElevator
 import frc.robot.subsystems.intake.IntakeSubsystem
 
-class SourceIntake: SequentialCommandGroup(
-    PositionElevator({ElevatorConstants.ELEVATOR_MIN_HEIGHT}, {it < 5.0}),
-    Commands.runOnce({IntakeSubsystem.intake()}, IntakeSubsystem)
+class SourceIntakeHome: SequentialCommandGroup(
+    Commands.runOnce({IntakeSubsystem.stopIntake()}, IntakeSubsystem),
+    PositionElevator({ElevatorConstants.L1_POSITION}, {it > 21.0})
 )
