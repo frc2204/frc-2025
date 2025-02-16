cd00674750b227d8b3b56bbccc5ca8e49080490f
Jiale Yu <yibai14651@gmail.com>
2025-02-15 21:53:45 -0800

    path type specification

--- src/main/java/frc/robot/path_finding_and_follow/PathFindingCommand.kt
+++ src/main/java/frc/robot/path_finding_and_follow/PathFindingCommand.kt
@@ -4,11 +4,12 @@ import com.pathplanner.lib.auto.AutoBuilder
 import com.pathplanner.lib.path.PathConstraints
 import com.pathplanner.lib.path.PathPlannerPath
 import edu.wpi.first.math.util.Units
+import edu.wpi.first.wpilibj2.command.Command
 
 object PathFindingCommand {
     private val intakePath = PathPlannerPath.fromPathFile("Auto_Align_Source.path")
     private val intakePathConstraints = PathConstraints(2.0, 3.0,
         Units.degreesToRadians(360.0), Units.degreesToRadians(540.0))
 
-    val intakePathFindingCommand = AutoBuilder.pathfindThenFollowPath(intakePath, intakePathConstraints)
+    val intakePathFindingCommand: Command = AutoBuilder.pathfindThenFollowPath(intakePath, intakePathConstraints)
 }
