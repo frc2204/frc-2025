880d7513952dcd41f12a244406a158c92f293b9d
Jiale Yu <yibai14651@gmail.com>
2025-02-19 19:24:50 -0800

    moved bb subsystem into ee subsystem

--- src/main/java/frc/robot/subsystems/end_effector/EESubsystem.kt
+++ src/main/java/frc/robot/subsystems/end_effector/EESubsystem.kt
@@ -2,18 +2,31 @@ package frc.robot.subsystems.end_effector
 
 import com.ctre.phoenix6.hardware.TalonFX
 import config.EEConstants
+import edu.wpi.first.wpilibj.DigitalInput
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import org.littletonrobotics.junction.Logger
 
 object EESubsystem: SubsystemBase() {
     private val eeMotor = TalonFX(EEConstants.EE_MOTOR_ID, "rio")
+    private val beamBreak = DigitalInput(1)
+    val beamBreakState
+        get() = beamBreak.get()
 
     init {
         eeMotor.configurator.apply(EECTREConfig.eeFXConfig)
     }
 
+    fun endEffectorStart() {
+        eeMotor.set(0.5)
+    }
+
+    fun endEffectorStop() {
+        eeMotor.stopMotor()
+    }
+
     override fun periodic() {
         Logger.recordOutput("EndEffectorVelocity", eeMotor.velocity.valueAsDouble)
         Logger.recordOutput("EndEffectorCurrent", eeMotor.torqueCurrent.valueAsDouble)
+        Logger.recordOutput("BeamBreakState", beamBreakState)
     }
 }
