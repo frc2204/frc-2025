98bf9191fd3dbd140a613ae4a96de7e5b981450b
plainriceeater <therealasher09@gmail.com>
2025-02-22 14:56:47 -0800

    Made GroundintakeSparkMAXConfig

---
+++ src/main/java/frc/robot/subsystems/groundintake/GroundIntakeSparkMAXConfig.kt
@@ -0,0 +1,22 @@
+package frc.robot.subsystems.intake
+
+import com.revrobotics.spark.config.SparkBaseConfig
+import com.revrobotics.spark.config.SparkMaxConfig
+
+object GroundIntakeSparkMAXConfig {
+    val groundIntakeMotorConfig = SparkMaxConfig()
+    init {
+        groundIntakeMotorConfig.smartCurrentLimit(40)
+        groundIntakeMotorConfig.idleMode(SparkBaseConfig.IdleMode.kBrake)
+        groundIntakeMotorConfig.inverted(true)
+    }
+}
+object GroundIntakeSparkMAXConfig2 {
+    val groundIntakeMotorConfig = SparkMaxConfig()
+    init {
+        groundIntakeMotorConfig.smartCurrentLimit(40)
+        groundIntakeMotorConfig.idleMode(SparkBaseConfig.IdleMode.kBrake)
+        groundIntakeMotorConfig.inverted(true)
+    }
+}
+
