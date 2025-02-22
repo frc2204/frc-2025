57c87dc20a968b096aee4df1663c7a633a757098
Jiale Yu <yibai14651@gmail.com>
2025-02-22 15:03:46 -0800

    made talonfx config

---
+++ src/main/java/frc/robot/subsystems/algae/TalonFXConfig.kt
@@ -0,0 +1,10 @@
+package frc.robot.subsystems.algae
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration
+
+object TalonFXConfig {
+    var AlgaeFXConfig = TalonFXConfiguration()
+    init{
+        AlgaeFXConfig = TalonFXConfiguration()
+    }
+}
