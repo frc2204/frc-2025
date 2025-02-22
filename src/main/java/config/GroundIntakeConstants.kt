a39b22968fdc53953cf5235d997f2fce09521b2d
plainriceeater <therealasher09@gmail.com>
2025-02-22 14:57:00 -0800

    Made GroundIntakeConstants

---
+++ src/main/java/config/GroundIntakeConstants.kt
@@ -0,0 +1,9 @@
+package config
+
+
+object GroundIntakeConstants{
+  const val GROUNDINTAKE_MOTOR_CAN_ID = 0 //change later
+  const val GROUNDINTAKE_SPEED = 0.0 //changer later
+  const val GROUNDINTAKE_MOTOR_ID = 0//change later
+//  const val G
+}
