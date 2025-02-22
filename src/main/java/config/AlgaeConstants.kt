3177b1e8e5474df993fd8af5149f4db9d51f0f06
Jiale Yu <yibai14651@gmail.com>
2025-02-22 14:59:20 -0800

    made algae intake constants

---
+++ src/main/java/config/AlgaeConstants.kt
@@ -0,0 +1,18 @@
+package config
+
+object AlgaeConstants {
+
+    const val ARM_MOTOR_ID = 0
+
+    const val LEFT_INTAKE_MOTOR_CAN_ID = 1
+    const val LEFT_INTAKE_SPEED = 0.5
+
+    const val RIGHT_INTAKE_MOTOR_CAN_ID = 2
+    const val RIGHT_INTAKE_SPEED = -0.5
+
+    const val PROCESSOR_POSITION = 7.0
+
+    const val ARM_MIN_ANGLE = 0.0
+    const val ARM_MAX_ANGLE = 90.0
+
+}
