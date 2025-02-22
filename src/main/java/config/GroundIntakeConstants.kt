4e5199cb1775ef488d0ca2479e88bf98de5860ba
plainriceeater <therealasher09@gmail.com>
2025-02-22 15:03:08 -0800

    Added Groundintake stalling current

--- src/main/java/config/GroundIntakeConstants.kt
+++ src/main/java/config/GroundIntakeConstants.kt
@@ -5,5 +5,6 @@ object GroundIntakeConstants{
   const val GROUNDINTAKE_MOTOR_CAN_ID = 0 //change later
   const val GROUNDINTAKE_SPEED = 0.0 //changer later
   const val GROUNDINTAKE_MOTOR_ID = 0//change later
+  const val GROUNDINTAKE_STALLING_CURRENT = 0 //change later
 //  const val G
 }
