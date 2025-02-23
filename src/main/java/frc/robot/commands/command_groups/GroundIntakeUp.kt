cbef08203012ce0d43eaa2114332bb6757dbe55d
plainriceeater <therealasher09@gmail.com>
2025-02-22 18:05:20 -0800

    made groundintake up

---
+++ src/main/java/frc/robot/commands/command_groups/GroundIntakeUp.kt
@@ -0,0 +1,16 @@
+package frc.robot.commands.command_groups
+
+
+import config.ElevatorConstants
+import config.GroundIntakeConstants
+import edu.wpi.first.wpilibj2.command.Commands
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
+import frc.robot.commands.algaeintake.PositionAlgaeIntake
+import frc.robot.subsystems.groundintake.GroundIntakeSubsystem
+
+// TODO: Add your sequential commands in the super constructor call,
+//       e.g. SequentialCommandGroup(OpenClawCommand(), MoveArmCommand())
+class GroundIntakeUp : SequentialCommandGroup(
+    PositionAlgaeIntake({ GroundIntakeConstants.GROUNDINTAKE_MAX_ANGLE}, {it < 0.0}),//change later
+    Commands.runOnce({GroundIntakeSubsystem.stopintake()},GroundIntakeSubsystem),
+)
