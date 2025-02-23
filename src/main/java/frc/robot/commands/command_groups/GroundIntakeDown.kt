ad34576748ca4a9dffa87909a703ea912c91c022
plainriceeater <therealasher09@gmail.com>
2025-02-22 18:03:47 -0800

    imported groundintakeconstants and position algae intake and added a command to lower the algae intake arm

--- src/main/java/frc/robot/commands/command_groups/GroundIntakeDown.kt
+++ src/main/java/frc/robot/commands/command_groups/GroundIntakeDown.kt
@@ -2,8 +2,11 @@ package frc.robot.commands.command_groups
 
 
 import config.ElevatorConstants
+import config.GroundIntakeConstants
+import config.IntakeConstants
 import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
+import frc.robot.commands.algaeintake.PositionAlgaeIntake
 import frc.robot.commands.elevator.PositionElevator
 import frc.robot.subsystems.groundintake.GroundIntakeSubsystem
 
@@ -11,5 +14,6 @@ import frc.robot.subsystems.groundintake.GroundIntakeSubsystem
 //       e.g. SequentialCommandGroup(OpenClawCommand(), MoveArmCommand())
 class GroundIntakeDown : SequentialCommandGroup(
     PositionElevator({ElevatorConstants.ELEVATOR_MIN_HEIGHT}, {it <1.0}),
+    PositionAlgaeIntake({GroundIntakeConstants.GROUNDINTAKE_MIN_ANGLE}, {it < 0.0}),//change later
     Commands.runOnce({GroundIntakeSubsystem.intake()},GroundIntakeSubsystem)
 )
