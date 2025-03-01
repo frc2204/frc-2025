3c6bc4986aff99faf37518a93e3da4728ae1a063
Jiale Yu <yibai14651@gmail.com>
2025-03-01 13:10:55 -0800

    test changes 3/1

--- src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.kt
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.kt
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.elevator
 
+import com.ctre.phoenix6.controls.PositionDutyCycle
 import com.ctre.phoenix6.hardware.TalonFX
 import config.ElevatorConstants
 import edu.wpi.first.wpilibj2.command.SubsystemBase
@@ -10,7 +11,7 @@ object ElevatorSubsystem: SubsystemBase() {
 
     val position get() = elevatorMotor.position.valueAsDouble
 
-    var desiredPosition = 0.0
+    var desiredPosition = position
         set(position) {
             field = if(position in ElevatorConstants.ELEVATOR_MIN_HEIGHT..ElevatorConstants.ELEVATOR_MAX_HEIGHT)
                 position
@@ -18,20 +19,19 @@ object ElevatorSubsystem: SubsystemBase() {
                 this.position
         }
 
-    var extensionOffset = 0.5
-
     init{
         elevatorMotor.configurator.apply(CTREConfig.elevatorFXConfig)
     }
 
     override fun periodic() {
-        elevatorMotor.setPosition(desiredPosition)
+        elevatorMotor.setControl(PositionDutyCycle(desiredPosition))
 
         Logger.recordOutput("ElevatorDesiredPosition", desiredPosition)
 
         Logger.recordOutput("ElevatorPosition", position)
         Logger.recordOutput("ElevatorVelocity", elevatorMotor.velocity.valueAsDouble)
         Logger.recordOutput("ElevatorCurrent", elevatorMotor.torqueCurrent.valueAsDouble)
+        Logger.recordOutput("ElevatorControlMode", elevatorMotor.controlMode.value)
     }
 
 }
