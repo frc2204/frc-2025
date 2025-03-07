69edb547b026b423fded4b6918e6167a5fc0d5d5
Jiale Yu <yibai14651@gmail.com>
2025-03-06 17:50:32 -0800

    drive.java pp changes robot moi

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -69,7 +69,7 @@ public class Drive extends SubsystemBase {
 
   // PathPlanner config constants
   private static final double ROBOT_MASS_KG = 56.699;
-  private static final double ROBOT_MOI = 6.883;
+  private static final double ROBOT_MOI = 4.11642;
   private static final double WHEEL_COF = 1.2;
   private static final RobotConfig PP_CONFIG =
       new RobotConfig(
