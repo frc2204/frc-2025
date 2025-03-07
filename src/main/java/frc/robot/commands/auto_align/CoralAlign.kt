cf01ff4b0a49cb36b0f5b11e43066f536775c5de
plainriceeater <therealasher09@gmail.com>
2025-03-06 23:23:28 -0800

    Made it so CANdle will change accoring to if CoralAlign is running

--- src/main/java/frc/robot/commands/auto_align/CoralAlign.kt
+++ src/main/java/frc/robot/commands/auto_align/CoralAlign.kt
@@ -1,10 +1,12 @@
 package frc.robot.commands.auto_align
 
 import config.AutoAlignConstants
+import config.LEDConstants
 import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.wpilibj2.command.Command
 import frc.robot.commands.DriveCommands
 import frc.robot.subsystems.drive.Drive
+import frc.robot.subsystems.led.LEDSubsystem
 import frc.robot.subsystems.vision.Vision
 import kotlin.math.PI
 
@@ -22,11 +24,13 @@ class CoralAlign(
     }
 
     override fun execute() {
+        LEDSubsystem.state = LEDConstants.IS_AUTO_ALIGNING
        calculatedTheta = degreesToRadians(calculateOffset())
        DriveCommands.joystickDriveAtAngle(driveSubsystem, {0.0}, {0.0}, {Rotation2d(calculatedTheta)})
     }
 
     override fun isFinished(): Boolean {
+        LEDSubsystem.state = LEDConstants.IDLE
        return false
     }
 
