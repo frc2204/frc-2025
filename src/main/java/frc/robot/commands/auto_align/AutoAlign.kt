ac313d25eb70295680e9a99a3a512d06c6d6c7f7
plainriceeater <therealasher09@gmail.com>
2025-03-06 23:19:07 -0800

    Made it so CANdle will change accoring to if AutoAlign is running

--- src/main/java/frc/robot/commands/auto_align/AutoAlign.kt
+++ src/main/java/frc/robot/commands/auto_align/AutoAlign.kt
@@ -2,10 +2,18 @@ package frc.robot.commands.auto_align
 
 import com.pathplanner.lib.auto.AutoBuilder
 import config.CompletePath
+import config.LEDConstants
 import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.InstantCommand
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
+import frc.robot.subsystems.led.LEDSubsystem
 
 object AutoAlign {
     fun pathFind(path: CompletePath): Command {
-        return AutoBuilder.pathfindThenFollowPath(path.path, path.constraints)
+        return SequentialCommandGroup(
+            InstantCommand({ LEDSubsystem.state = LEDConstants.IS_AUTO_ALIGNING }, LEDSubsystem),
+            AutoBuilder.pathfindThenFollowPath(path.path, path.constraints),
+            InstantCommand({ LEDSubsystem.state = LEDConstants.IDLE }, LEDSubsystem)
+        )
+        }
     }
-}
