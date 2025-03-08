4368ac9f34721d77bb2f8dc853cca62faa0e6ea6
Jiale Yu <yibai14651@gmail.com>
2025-03-08 12:03:26 -0800

    swerve pid tuning

--- src/main/java/config/TunerConstants.java
+++ src/main/java/config/TunerConstants.java
@@ -21,7 +21,7 @@ public class TunerConstants {
   // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
   private static final Slot0Configs steerGains =
       new Slot0Configs()
-          .withKP(12.0)
+          .withKP(3)
           .withKI(0)
           .withKD(0.0)
           .withKS(0.1)
@@ -31,7 +31,7 @@ public class TunerConstants {
   // When using closed-loop control, the drive motor uses the control
   // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
   private static final Slot0Configs driveGains =
-      new Slot0Configs().withKP(2.75).withKI(0).withKD(0).withKS(0.16380).withKV(0.85753);
+      new Slot0Configs().withKP(1.5).withKI(0).withKD(0).withKS(0.16380).withKV(0.85753);
 
   // The closed-loop output type to use for the steer motors;
   // This affects the PID/FF gains for the steer motors
