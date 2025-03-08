495a37741427039eca81cd6a946c84417d486d73
Jiale Yu <yibai14651@gmail.com>
2025-03-08 01:09:59 -0800

    candle changes

--- src/main/java/frc/robot/commands/command_groups/SourceIntake.kt
+++ src/main/java/frc/robot/commands/command_groups/SourceIntake.kt
@@ -1,14 +1,17 @@
 package frc.robot.commands.command_groups
 
 import config.ElevatorConstants
+import config.StatusConstants
 import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
 import frc.robot.commands.elevator.PositionElevator
 import frc.robot.commands.end_effector.BeamBreakCommand
 import frc.robot.subsystems.end_effector.EESubsystem
 import frc.robot.subsystems.intake.IntakeSubsystem
+import frc.robot.subsystems.status.StatusSubsystem
 
 class SourceIntake: SequentialCommandGroup(
+    Commands.runOnce({StatusSubsystem.setCandleStatus(StatusConstants.intakeStatus)}, StatusSubsystem),
     PositionElevator({ElevatorConstants.ELEVATOR_MIN_HEIGHT},
         {it in ElevatorConstants.ELEVATOR_MIN_HEIGHT - ElevatorConstants.OFFSET_RATE..ElevatorConstants.ELEVATOR_MIN_HEIGHT + ElevatorConstants.OFFSET_RATE}),
     Commands.runOnce({IntakeSubsystem.intake()}, IntakeSubsystem),
