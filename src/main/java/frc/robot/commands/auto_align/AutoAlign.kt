ba6547596d8ad6052dd1180acb8bde52ab5564ff
Jiale Yu <yibai14651@gmail.com>
2025-03-09 19:23:42 -0700

    post-localization optimization 3/9

--- src/main/java/frc/robot/commands/auto_align/AutoAlign.kt
+++ src/main/java/frc/robot/commands/auto_align/AutoAlign.kt
@@ -7,10 +7,12 @@ import frc.robot.subsystems.vision.VisionConstants
 
 object AutoAlign {
     fun pathFind(path: CompletePath, pathType: PathType): Command {
-        if(pathType == PathType.SCORE)
-            VisionConstants.cameraStdDevFactors[1] = 0.0
-        if(pathType == PathType.SOURCE)
-            VisionConstants.cameraStdDevFactors[0] = 0.0
+//        if(pathType == PathType.SCORE)
+//            VisionConstants.cameraStdDevFactors = DoubleArray(2) { if (it == 0) 1.0 else 0.0 }
+//            println()
+//        if(pathType == PathType.SOURCE)
+//            VisionConstants.cameraStdDevFactors = DoubleArray(2) { if (it == 0) 0.0 else 1.0 }
+//            println()
 
         return AutoBuilder.pathfindThenFollowPath(path.path, path.constraints)
     }
