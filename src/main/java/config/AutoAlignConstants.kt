aff4f23b7a08ed24b252768aff615df920aa2673
Jiale Yu <yibai14651@gmail.com>
2025-03-12 16:50:15 -0700

    autonomous paths mapping

--- src/main/java/config/AutoAlignConstants.kt
+++ src/main/java/config/AutoAlignConstants.kt
@@ -23,6 +23,30 @@ object AutoAlignConstants {
     val ALIGN_REEF4_Left = CompletePath(PathPlannerPath.fromPathFile("Auto_Align_Reef4_Left"),
         PathConstraints(1.0, 1.5,
             Units.degreesToRadians(360.0), Units.degreesToRadians(540.0)))
+
+    val ALIGN_REEF1_Left = CompletePath(PathPlannerPath.fromPathFile("Auto_Align_Reef1_Left"),
+        PathConstraints(1.0, 1.5,
+            Units.degreesToRadians(360.0), Units.degreesToRadians(540.0)))
+
+    val ALIGN_REEF1_Right = CompletePath(PathPlannerPath.fromPathFile("Auto_Align_Reef1_Right"),
+        PathConstraints(1.0, 1.5,
+            Units.degreesToRadians(360.0), Units.degreesToRadians(540.0)))
+
+    val ALIGN_REEF2_Left = CompletePath(PathPlannerPath.fromPathFile("Auto_Align_Reef2_Left"),
+        PathConstraints(1.0, 1.5,
+            Units.degreesToRadians(360.0), Units.degreesToRadians(540.0)))
+
+    val ALIGN_REEF2_Right = CompletePath(PathPlannerPath.fromPathFile("Auto_Align_Reef2_Right"),
+        PathConstraints(1.0, 1.5,
+            Units.degreesToRadians(360.0), Units.degreesToRadians(540.0)))
+
+    val ALIGN_REEF6_Left = CompletePath(PathPlannerPath.fromPathFile("Auto_Align_Reef6_Left"),
+        PathConstraints(1.0, 1.5,
+            Units.degreesToRadians(360.0), Units.degreesToRadians(540.0)))
+
+    val ALIGN_REEF6_Right = CompletePath(PathPlannerPath.fromPathFile("Auto_Align_Reef6_Right"),
+        PathConstraints(1.0, 1.5,
+            Units.degreesToRadians(360.0), Units.degreesToRadians(540.0)))
 }
 
 data class CompletePath(val path: PathPlannerPath, val constraints: PathConstraints)
