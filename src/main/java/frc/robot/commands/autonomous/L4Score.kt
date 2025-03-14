2cd154153e78a96d115b7b17bebd17bb1981c04d
Jiale Yu <yibai14651@gmail.com>
2025-03-14 10:08:12 -0700

    add autonomous L4 coral scoring

---
+++ src/main/java/frc/robot/commands/autonomous/L4Score.kt
@@ -0,0 +1,19 @@
+package frc.robot.commands.autonomous
+
+import config.ElevatorConstants
+import edu.wpi.first.wpilibj2.command.Commands
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
+import edu.wpi.first.wpilibj2.command.WaitCommand
+import frc.robot.commands.elevator.PositionElevator
+import frc.robot.subsystems.end_effector.EESubsystem
+
+class L4Score(): SequentialCommandGroup(
+    PositionElevator({ ElevatorConstants.L4_POSITION },
+        {it in ElevatorConstants.L4_POSITION - ElevatorConstants.SCORING_OFFSET .. ElevatorConstants.L4_POSITION + ElevatorConstants.SCORING_OFFSET}),
+    Commands.runOnce({ EESubsystem.eeScore() }, EESubsystem),
+    WaitCommand(0.5),
+    Commands.runOnce({ EESubsystem.stopEndEffector() }, EESubsystem),
+    WaitCommand(0.2),
+    PositionElevator({ ElevatorConstants.ELEVATOR_MIN_HEIGHT },
+        {it in ElevatorConstants.ELEVATOR_MIN_HEIGHT - ElevatorConstants.OFFSET_RATE..ElevatorConstants.ELEVATOR_MIN_HEIGHT + ElevatorConstants.OFFSET_RATE})
+)
