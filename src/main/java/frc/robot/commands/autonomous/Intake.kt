d863a7593e2dc116fbe3af54408cf6e155afa470
Jiale Yu <yibai14651@gmail.com>
2025-03-14 10:08:03 -0700

    add autonomous intake

---
+++ src/main/java/frc/robot/commands/autonomous/Intake.kt
@@ -0,0 +1,17 @@
+package frc.robot.commands.autonomous
+
+import config.ElevatorConstants
+import edu.wpi.first.wpilibj2.command.Commands
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
+import frc.robot.commands.elevator.PositionElevator
+import frc.robot.commands.end_effector.BeamBreakCommand
+import frc.robot.subsystems.end_effector.EESubsystem
+import frc.robot.subsystems.intake.IntakeSubsystem
+
+class Intake: SequentialCommandGroup(
+    PositionElevator({ ElevatorConstants.ELEVATOR_MIN_HEIGHT},
+        {it in ElevatorConstants.ELEVATOR_MIN_HEIGHT - ElevatorConstants.OFFSET_RATE..ElevatorConstants.ELEVATOR_MIN_HEIGHT + ElevatorConstants.OFFSET_RATE}),
+    Commands.runOnce({ IntakeSubsystem.intake()}, IntakeSubsystem),
+    Commands.runOnce({ EESubsystem.startEndEffector()}, EESubsystem),
+    BeamBreakCommand ({ EESubsystem.beamBreakState }, { IntakeSubsystem.intakeCurrent })
+)
