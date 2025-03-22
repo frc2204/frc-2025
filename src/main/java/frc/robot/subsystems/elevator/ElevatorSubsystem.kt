b6605cae7a513b3b61ba36d130d5a453c317ab07
Jiale Yu <yibai14651@gmail.com>
2025-03-22 10:59:55 -0700

    implement through bore for elevator

--- src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.kt
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.kt
@@ -3,6 +3,7 @@ package frc.robot.subsystems.elevator
 import com.ctre.phoenix6.controls.PositionDutyCycle
 import com.ctre.phoenix6.hardware.TalonFX
 import config.ElevatorConstants
+import edu.wpi.first.wpilibj.DutyCycleEncoder
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import org.littletonrobotics.junction.Logger
 
@@ -10,6 +11,8 @@ object ElevatorSubsystem: SubsystemBase() {
     private val elevatorMotor = TalonFX(ElevatorConstants.ELEVATOR_MOTOR_ID, "rio")
 
     val position get() = elevatorMotor.position.valueAsDouble
+
+    private val throughBorePosition = DutyCycleEncoder(1)
 
     var desiredPosition = position
         set(position) {
@@ -21,6 +24,7 @@ object ElevatorSubsystem: SubsystemBase() {
 
     init{
         elevatorMotor.configurator.apply(CTREConfig.elevatorFXConfig)
+        throughBorePosition.setDutyCycleRange(0.0, 1.0)
     }
 
     override fun periodic() {
@@ -34,6 +38,9 @@ object ElevatorSubsystem: SubsystemBase() {
         Logger.recordOutput("ElevatorVelocity", elevatorMotor.velocity.valueAsDouble)
         Logger.recordOutput("ElevatorCurrent", elevatorMotor.torqueCurrent.valueAsDouble)
         Logger.recordOutput("ElevatorControlMode", elevatorMotor.controlMode.value)
+
+        print("through bore: " + throughBorePosition.get())
+        print("through bore converted: " + ((throughBorePosition.get() / 360.0)) * 2048.0)
     }
 
 }
