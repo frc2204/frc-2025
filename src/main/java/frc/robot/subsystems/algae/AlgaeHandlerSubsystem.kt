80e2964a09902ecee51db48e2f59d8d55c6ccbdd
Jiale Yu <yibai14651@gmail.com>
2025-03-26 11:10:44 -0700

    configure motor objects AlgaeHandlerSubsystem

--- src/main/java/frc/robot/subsystems/algae/AlgaeHandlerSubsystem.kt
+++ src/main/java/frc/robot/subsystems/algae/AlgaeHandlerSubsystem.kt
@@ -1,4 +1,18 @@
 package frc.robot.subsystems.algae
 
-object AlgaeHandlerSubsystem {
+import com.ctre.phoenix6.hardware.TalonFX
+import com.revrobotics.spark.SparkBase
+import com.revrobotics.spark.SparkLowLevel
+import com.revrobotics.spark.SparkMax
+import config.AlgaeHandlerConstants
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+
+object AlgaeHandlerSubsystem: SubsystemBase() {
+    private val spinner = SparkMax(AlgaeHandlerConstants.ALGAE_HANDLER_SPINNER_ID, SparkLowLevel.MotorType.kBrushless).apply {
+        configure(AlgaeHandlerSparkMAXConfig.algaeSpinnerConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters)
+    }
+
+    private val wrist = TalonFX(AlgaeHandlerConstants.ALGAE_HANDLER_WRIST_ID, "rio").apply {
+        configurator.apply(AlgaeHandlerCTREConfig.algaeWristFXConfig)
+    }
 }
