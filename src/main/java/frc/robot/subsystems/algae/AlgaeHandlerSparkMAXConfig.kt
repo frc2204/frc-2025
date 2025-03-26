2c7079329289ee71dc990ff63c5ae9ac9896879b
Jiale Yu <yibai14651@gmail.com>
2025-03-26 11:10:14 -0700

    add algae spinner sparkmax config

--- src/main/java/frc/robot/subsystems/algae/AlgaeHandlerSparkMAXConfig.kt
+++ src/main/java/frc/robot/subsystems/algae/AlgaeHandlerSparkMAXConfig.kt
@@ -1,4 +1,18 @@
 package frc.robot.subsystems.algae
 
+import com.revrobotics.spark.config.SparkBaseConfig
+import com.revrobotics.spark.config.SparkMaxConfig
+import config.AlgaeHandlerConstants
+
 object AlgaeHandlerSparkMAXConfig {
+    val algaeSpinnerConfig = SparkMaxConfig()
+
+    init {
+        algaeSpinnerConfig.smartCurrentLimit(40)
+        algaeSpinnerConfig.idleMode(SparkBaseConfig.IdleMode.kCoast)
+        /** verification required */
+        algaeSpinnerConfig.inverted(false)
+        algaeSpinnerConfig.openLoopRampRate(0.25)
+        algaeSpinnerConfig.encoder.positionConversionFactor(AlgaeHandlerConstants.ALGAE_HANDLER_SPINNER_REDUCTION)
+    }
 }
