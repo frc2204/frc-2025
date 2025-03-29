7301f2583453bf28f5252b1bd024084121716104
Jiale Yu <yibai14651@gmail.com>
2025-03-28 19:04:34 -0700

    add algae zero wrist position on robot start logic

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -16,11 +16,13 @@ package frc.robot;
 import com.ctre.phoenix6.swerve.SwerveModuleConstants;
 import com.ctre.phoenix6.swerve.SwerveModuleConstants.DriveMotorArrangement;
 import com.ctre.phoenix6.swerve.SwerveModuleConstants.SteerMotorArrangement;
+import config.AlgaeHandlerConstants;
 import config.ElevatorConstants;
 import config.TunerConstants;
 import edu.wpi.first.wpilibj.Threads;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.algae.AlgaeHandlerSubsystem;
 import frc.robot.subsystems.elevator.ElevatorSubsystem;
 import org.littletonrobotics.junction.LogFileUtil;
 import org.littletonrobotics.junction.LoggedRobot;
@@ -41,6 +43,7 @@ public class Robot extends LoggedRobot {
 
   public Robot() {
     ElevatorConstants.INSTANCE.setElevatorMinHeight(ElevatorSubsystem.INSTANCE.getPosition());
+    AlgaeHandlerConstants.INSTANCE.setWristMinPosition(AlgaeHandlerSubsystem.INSTANCE.getCurrentPosition());
 
     // Record metadata
     Logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
